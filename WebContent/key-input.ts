a720be274cbcc21a383a37bfa5db09a13ebfa457
dickson-hong <dickson.y.hong@gmail.com>
2025-12-01 10:47:28 -0800

    Implement letter/word spacing

--- WebContent/key-input.ts
+++ WebContent/key-input.ts
@@ -1,3 +1,5 @@
+import { MorseMeta, MorseState, MorseAudio, Keytype } from './morse-classes.js';
+
 const morseCodeMap = {
     'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.',
     'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
@@ -8,44 +10,37 @@ const morseCodeMap = {
     '4': '....-', '5': '.....', '6': '-....', '7': '--...', '8': '---..',
     '9': '----.'
 };
-const Keytype = { iambic: 'Iambic', straight: 'Straight' };
 
-const wpm: number = 15;
-let keytype: string = Keytype.straight;
-let controls = { 'straight': ' ', 'iambicShort': 'z', 'iambicLong': 'x' };
-let times = wpmToMorseTimes(wpm);
 
-let context: AudioContext = new AudioContext();
-var osc: OscillatorNode;
+var morseMeta = new MorseMeta();
+var morseState = new MorseState();
+var morseAudio = new MorseAudio();
 
+//document.addEventListener('keydown', interruptTimer);
 document.addEventListener('keydown', handleKeydown);
 document.addEventListener('keyup', handleKeyup);
 
 
-function getOsc() {
-    var osc: OscillatorNode = context.createOscillator();
-    osc.type = 'sine';
-    osc.frequency.value = 600;
-    osc.connect(context.destination);
-    return osc;
-}
-
 function handleKeydown(e: KeyboardEvent) {
+    // Stop timers if they are running
+    resetTimers(morseState);
+
     console.log(e.key);
-    if (keytype == Keytype.straight && e.key == controls.straight && !e.repeat) {
+    if (morseMeta.keytype == Keytype.straight &&
+        e.key == morseMeta.controls.straight && !e.repeat) {
         straightDown();
     }
-    else {
+    else if (morseMeta.keytype == Keytype.iambic &&
+        (e.key == morseMeta.controls.iambicShort || e.key == morseMeta.controls.iambicLong) && !e.repeat) {
         iambicDown();
     }
 }
-
 function handleKeyup(e: KeyboardEvent) {
-    console.log(wpmToMorseTimes(wpm).dot);
-    if (keytype == Keytype.straight && e.key == controls.straight) {
+    if (morseMeta.keytype == Keytype.straight && e.key == morseMeta.controls.straight) {
         straightUp();
     }
-    else {
+    else if (morseMeta.keytype == Keytype.iambic &&
+        (e.key == morseMeta.controls.iambicShort || e.key == morseMeta.controls.iambicLong)) {
         iambicUp();
     }
 }
@@ -54,22 +49,55 @@ function iambicDown() { }
 function iambicUp() { }
 
 function straightDown() {
-    osc = getOsc();
-    osc.start();
+    morseState.startTime = Date.now();
+    morseAudio.startOsc();
 }
 function straightUp() {
-    osc.stop();
+    if (!morseState.startTime) {
+        return;
+    }
+    morseState.endTime = Date.now();
+    let pressDuration = Math.min(morseState.endTime - morseState.startTime);
+    let display = $('#ditsAndDahs');
+    if (pressDuration < morseMeta.times.dash) {
+        // Handle dot
+        display.text('.');
+        morseState.currLetterMorse += '.';
+    } else {
+        // Handle dash
+        display.text('-');
+        morseState.currLetterMorse += '-';
+    }
+    morseAudio.stopOsc();
+    // Reset startTime: Down-up pair complete
+    morseState.startTime = null;
+
+    // Write current letter
+    morseState.letterTimer = setTimeout(letterTimeout, morseMeta.times.letterGap);
+    // Write a space
+    morseState.wordTimer = setTimeout(spaceTimeout, morseMeta.times.wordGap);
 }
 
-function wpmToMorseTimes(wpm: number) {
-    if (wpm < 1) wpm = 1
-    if (wpm > 200) wpm = 200
-    const dotTime = Math.ceil(1200 / wpm)
-    return {
-        dot: dotTime,
-        dash: dotTime * 3,
-        elementGap: dotTime,
-        letterGap: dotTime * 3,
-        wordGap: dotTime * 7
+function letterTimeout() {
+    let display = $('#letters');
+    // CONVERT MORSE TO LETTER HERE.
+    display.append('$');
+}
+
+function spaceTimeout() {
+    let display = $('#letters');
+    display.append(' ');
+}
+
+function resetTimers(morseState: MorseState) {
+    if (morseState.letterTimer) {
+        clearTimeout(morseState.letterTimer);
+        morseState.letterTimer = null;
+    }
+    if (morseState.wordTimer) {
+        clearTimeout(morseState.wordTimer);
+        morseState.wordTimer = null;
     }
 }
+
+
---
+++ WebContent/morse-classes.ts
@@ -0,0 +1,82 @@
+export enum Keytype {
+    iambic = 'Iambic',
+    straight = 'Straight'
+}
+
+export class MorseMeta {
+    private _wpm: number = 15;
+    private _keytype: Keytype = Keytype.straight;
+    private _controls = { straight: ' ', iambicShort: 'z', iambicLong: 'x' };
+    private _times = wpmToMorseTimes(this._wpm);
+
+    get wpm() {
+        return this._wpm;
+    }
+
+    get keytype() {
+        return this._keytype;
+    }
+
+    get controls() {
+        return this._controls;
+    }
+
+    get times() {
+        return this._times;
+    }
+
+    set wpm(newWpm: number) {
+        this._wpm = newWpm;
+        this._times = wpmToMorseTimes(this._wpm);
+    }
+
+    set keytype(newKeytype: Keytype) {
+        this._keytype = newKeytype;
+    }
+}
+
+export class MorseState {
+    startTime: number | null = null;
+    endTime: number | null = null;
+    letterTimer: number | null = null;
+    wordTimer: number | null = null;
+
+    currLetterMorse: string = '';
+}
+
+export class MorseAudio {
+    private context: AudioContext = new AudioContext();
+    private osc: OscillatorNode | null = null;
+
+    private makeOsc() {
+        var osc: OscillatorNode = this.context.createOscillator();
+        osc.type = 'sine';
+        osc.frequency.value = 600;
+        osc.connect(this.context.destination);
+        return osc;
+    }
+    startOsc() {
+        this.osc = this.makeOsc();
+        this.osc.start();
+    }
+    stopOsc() {
+        if (this.osc) {
+            this.osc.stop();
+            this.osc.disconnect();
+            this.osc = null;
+        }
+    }
+}
+
+function wpmToMorseTimes(wpm: number) {
+    if (wpm < 1) wpm = 1
+    if (wpm > 200) wpm = 200
+    const dotTime = Math.ceil(1200 / wpm)
+    return {
+        dot: dotTime,
+        dash: dotTime * 3,
+        elementGap: dotTime,
+        letterGap: dotTime * 3,
+        wordGap: dotTime * 7
+    }
+}

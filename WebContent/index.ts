aae2d161573ac3ad40f01dc85e798ee36e586184
dickson-hong <dickson.y.hong@gmail.com>
2025-12-23 16:19:24 -0800

    Create basic menu page

---
+++ WebContent/index.ts
@@ -0,0 +1,11 @@
+$('#go-game').on('click', function () {
+    window.location.href = 'game.html';
+});
+
+$('go-settings').on('click', function () {
+    window.location.href = 'settings.html';
+});
+
+$('go-about').on('click', function () {
+    window.location.href = 'about.html';
+});
